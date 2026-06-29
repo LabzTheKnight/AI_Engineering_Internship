e656e9af0e5d63a9558f718a87cbc5f2d7b5f12b
Afolabi <aaafolayan@gmail.com>
2026-06-29 15:18:26 +0200

    crud fast api for phase 1 no error handling no basic json db

--- projects/phase-1/main.py
+++ projects/phase-1/main.py
@@ -14,7 +14,7 @@ def add(a,file_path = filename) :
         task = { "index": index , "title": a , "completed": False }
         data.append(task)
     with open(file_path, "w") as file :
-        json.dump( data , file )
+        json.dump( data , file , indent=4)
 
 def list(file_path = filename):
     with open( file_path, "r" ) as file :
@@ -25,15 +25,32 @@ def list_id(id: int ,file_path: str = filename):
     return  list(file_path)[id - 1]
     
 
+def complete(index , file_path = filename):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+        task = data[index - 1]
+        task["completed"] = True
+        data[index - 1] = task
+    with open(file_path , "w") as file:
+        json.dump(data, file , indent=4)
 
-
+def delete(index , file_path = filename):
+    with open(file_path , "r") as file:
+        index = index - 1
+        data = json.load(file)
+        data.pop(index)
+        i = 1
+        for task in data:
+            task["index"] = i
+            i+=1
+    with open(file_path , "w") as file:
+        json.dump(data, file , indent= 4)
 
 
 @app.get("/")
 def read_root():
     return {"Hello World"}
 
-@app.get("/health")
 
 @app.get("/items")
 def read_items():
@@ -43,3 +60,15 @@ def read_items():
 def read_item(item_id: int, q: str | None = None):
     return list_id(item_id)
 
+@app.post("/items/new")
+def save_item(item_name: str):
+    add(item_name)
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int):
+    complete(item_id)
+    
+    
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    delete(item_id)
